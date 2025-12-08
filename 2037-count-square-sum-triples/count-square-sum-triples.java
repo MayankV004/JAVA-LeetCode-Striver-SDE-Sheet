9d24b7d8c96ae944f2c404fdf614b62b9cc97c0d
Mayank Verma <mayank.msverma@gmail.com>
2025-12-09 01:12:15 +0530

    Time: 7 ms (89.00%) | Memory: 42 MB (50.83%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -2,15 +2,27 @@ class Solution {
 
     public int countTriples(int n) {
         int ans = 0;
-        for(int i = 1 ; i <= n-2; i++){
-            for(int j = i+1 ; j <= n-1 ; j++){
-                for(int k = j+1 ; k <= n ; k++){
-                    if((i*i) + (j*j) == k*k )
-                    {
-                        ans+=2;   
-                    }
-                }
+        // for(int i = 1 ; i <= n-2; i++){
+        //     for(int j = i+1 ; j <= n-1 ; j++){
+        //         for(int k = j+1 ; k <= n ; k++){
+        //             if((i*i) + (j*j) == k*k )
+        //             {
+        //                 ans+=2;   
+        //             }
+        //         }
+        //     }
+        // }
+        
+        for(int i = n ; i> 0 ; i--){
+            int left = 1;
+            int right = n-1;
+            while(left < right){
+                if(right * right + left*left > i*i) right--;
+                else left++;
+
+                if(right*right + left*left == i*i) ans+=2;
             }
+
         }
         return ans;
     }
