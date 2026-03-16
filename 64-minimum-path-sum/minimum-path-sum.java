027600c72fc02d877262d01ad24528e4a28f3547
Mayank Verma <mayank.msverma@gmail.com>
2026-03-17 01:35:44 +0530

    Time: 1 ms (99.75%) | Memory: 50.1 MB (89.50%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,61 @@
+//Memoization
+// class Solution {
+//     public int helper(int i , int j , int [][]grid , int dp[][]){
+//         if(i == 0 && j == 0 ) return grid[i][j];
+
+//         if(i < 0  || j < 0) return (int)1e9; // used to avoid overflow 1e9
+
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         int up = grid[i][j] + helper(i-1 , j , grid , dp);
+//         int left = grid[i][j] + helper(i , j-1 , grid , dp);
+
+//         return dp[i][j] = Math.min(up , left);
+        
+//     }
+
+//     public int minPathSum(int[][] grid) {
+        
+//         int m = grid.length;
+//         int n = grid[0].length;
+
+//         int dp[][] = new int[m][n];
+
+//         for(int i = 0 ; i < m ; i ++){
+//             Arrays.fill(dp[i] , -1);
+//         }
+
+//         return helper(m-1 , n-1 , grid , dp);
+//     }
+// }
+
+
+class Solution {
+    public int helper(int i , int j , int [][]grid , int dp[][]){
+        if(i == 0 && j == 0 ) return grid[i][j];
+
+        if(i < 0  || j < 0) return (int)1e9; // used to avoid overflow 1e9
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = grid[i][j] + helper(i-1 , j , grid , dp);
+        int left = grid[i][j] + helper(i , j-1 , grid , dp);
+
+        return dp[i][j] = Math.min(up , left);
+        
+    }
+
+    public int minPathSum(int[][] grid) {
+        
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int dp[][] = new int[m][n];
+
+        for(int i = 0 ; i < m ; i ++){
+            Arrays.fill(dp[i] , -1);
+        }
+
+        return helper(m-1 , n-1 , grid , dp);
+    }
+}
