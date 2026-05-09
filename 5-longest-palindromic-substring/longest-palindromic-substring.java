64f70b8961ca965e0cc48447ed31e392018583ff
Mayank Verma <mayank.msverma@gmail.com>
2026-05-09 17:41:47 +0530

    Time: 248 ms (17.22%) | Memory: 175.4 MB (5.10%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,36 +1,43 @@
 class Solution {
 
-    public boolean isPalindrome(String s , int i , int j , boolean [][]dp){
+    public boolean isPalindrome(String s, int i, int j, Boolean[][] dp) {
+
         if(i >= j) return true;
-        if(dp[i][j] != false) return dp[i][j];
+
+        if(dp[i][j] != null) {
+            return dp[i][j];
+        }
 
         if(s.charAt(i) == s.charAt(j)) {
-            return dp[i][j] = isPalindrome(s , i+1 , j-1, dp);
+            return dp[i][j] = isPalindrome(s, i + 1, j - 1, dp);
         }
+
         return dp[i][j] = false;
     }
 
     public String longestPalindrome(String s) {
 
         int n = s.length();
-        int maxLen = Integer.MIN_VALUE;
-        int start_idx = 0;
 
-        // using DP for optimization 
+        int maxLen = 0;
+        int start = 0;
 
-        boolean[][] dp = new boolean[1001][1001];
+        Boolean[][] dp = new Boolean[n][n];
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i ; j < n ; j++){
-                if(isPalindrome(s , i , j , dp)){
-                    if(j-i+1 > maxLen){
+        for(int i = 0; i < n; i++) {
+
+            for(int j = i; j < n; j++) {
+
+                if(isPalindrome(s, i, j, dp)) {
+
+                    if(j - i + 1 > maxLen) {
                         maxLen = j - i + 1;
-                        start_idx = i;
+                        start = i;
                     }
                 }
             }
         }
 
-        return s.substring(start_idx , start_idx + maxLen);   
+        return s.substring(start, start + maxLen);
     }
 }
