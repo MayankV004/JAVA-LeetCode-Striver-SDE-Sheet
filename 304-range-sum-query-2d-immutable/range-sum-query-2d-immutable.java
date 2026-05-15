558b57aa1d5b5e25ee86f1ee3fd67c53da86856e
Mayank Verma <mayank.msverma@gmail.com>
2026-05-15 16:29:35 +0530

    Time: 102 ms (77.09%) | Memory: 142.2 MB (24.67%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,5 +1,11 @@
 class NumMatrix {
 
+    // Prefix sum matrix
+    // p[i][j] stores sum of rectangle:
+    // (0,0) -> (i-1,j-1)
+    //
+    // Extra row and column are added
+    // to avoid boundary checks.
     private int[][] p;
 
     public NumMatrix(int[][] matrix) {
@@ -7,12 +13,19 @@ class NumMatrix {
         int m = matrix.length;
         int n = matrix[0].length;
 
+        // (m+1) x (n+1) prefix matrix
         p = new int[m + 1][n + 1];
 
+        // Build prefix sum matrix
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
 
-                p[i][j] = matrix[i - 1][j - 1]
+                // Current cell value
+                // + upper prefix
+                // + left prefix
+                // - overlap counted twice
+                p[i][j] =
+                        matrix[i - 1][j - 1]
                         + p[i - 1][j]
                         + p[i][j - 1]
                         - p[i - 1][j - 1];
@@ -21,6 +34,18 @@ class NumMatrix {
     }
 
     public int sumRegion(int r1, int c1, int r2, int c2) {
+
+        // Inclusion-Exclusion Principle
+        //
+        // total rectangle
+        // - upper extra area
+        // - left extra area
+        // + overlap removed twice
+        //
+        // NOTE:
+        // Prefix matrix is shifted by +1,
+        // so indices become:
+        // (r2+1, c2+1)
 
         return p[r2 + 1][c2 + 1]
                 - p[r1][c2 + 1]
@@ -31,6 +56,7 @@ class NumMatrix {
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
+ *
  * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ * int ans = obj.sumRegion(row1, col1, row2, col2);
  */
