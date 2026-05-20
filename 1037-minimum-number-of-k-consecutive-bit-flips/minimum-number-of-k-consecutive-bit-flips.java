ebc43b8837c8d313eb5308e14234f2ed83762834
Mayank Verma <mayank.msverma@gmail.com>
2026-05-21 02:32:56 +0530

    Time: 12 ms (22.26%) | Memory: 71.3 MB (99.65%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.java
@@ -1,28 +1,30 @@
 class Solution {
     public int minKBitFlips(int[] nums, int k) {
-        int flipCount = 0; // Tracks the current number of flips
-        int result = 0;    // Tracks the total number of flips performed
+        int n = nums.length;
+        int flips = 0;
+        int flipCount = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            // If the current index is outside the range of the last flip window, adjust flipCount
-            if (i >= k && nums[i - k] == -1) {
-                flipCount--;
+        Deque<Integer> dq = new LinkedList<>();
+
+        for(int i = 0 ; i < n ; i++){
+
+            if(i >= k){
+                flipCount -= dq.pollFirst();
             }
-            // If the current bit needs to be flipped to become 1
-            if (flipCount % 2 == nums[i]) {
-                // If flipping is not possible because the remaining elements are less than k
-                if (i + k > nums.length) {
-                    return -1;
-                }
-                // Mark the current position as flipped and update counters
-                nums[i] = -1;
-                // Increase the flip count
+
+            if(flipCount % 2 == nums[i]){
+                
+                if(i+k > n) return -1;
+
                 flipCount++;
-                // Increase the result
-                result++;
+                dq.addLast(1);
+                flips ++;
+
+            }else{
+                dq.addLast(0);
             }
         }
 
-        return result;
+        return flips;
     }
 }
