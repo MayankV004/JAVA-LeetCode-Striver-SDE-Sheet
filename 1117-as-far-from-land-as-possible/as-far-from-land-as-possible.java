0888d652dc6ef698d723c13b135b94a1ff6dd22c
Mayank Verma <mayank.msverma@gmail.com>
2026-05-25 12:06:16 +0530

    Time: 14 ms (90.58%) | Memory: 47.7 MB (13.94%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.java
@@ -3,25 +3,34 @@ class Solution {
         int n = grid.length;
 
         Queue<int[]> q = new ArrayDeque<>();
+        int visited[][] = new int[n][n];
+        int landCount = 0;
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++)
             {
                 if(grid[i][j] == 1){
-                    q.offer(new int[]{i , j , 1});
+                    q.offer(new int[]{i , j , 0});
+                    visited[i][j] = 1;
+                    landCount++;
                 }
             }
         }
+
+        // for all land or all water 
+        if(landCount == 0 || landCount == n * n){
+            return -1;
+        }
+
         int[] delRow = { 0, 0, -1, 1 };
         int[] delCol = { -1, 1, 0, 0 };
-        int distance = Integer.MIN_VALUE;
-        int visited[][] = new int[n][n];
+        int distance = 0;
         while(!q.isEmpty()){
             int []node = q.poll();
             int row = node[0];
             int col = node[1];
             int dist = node[2];
-
+            distance = Math.max(distance , dist);
             for(int i = 0 ; i < 4 ; i++){
                 int nrow = row + delRow[i];
                 int ncol = col + delCol[i];
@@ -29,11 +38,11 @@ class Solution {
                 if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < n && visited[nrow][ncol] == 0 && grid[nrow][ncol] == 0){
                     visited[nrow][ncol] = 1;
                     q.offer(new int[]{nrow , ncol , dist+1});
-                    distance = Math.max(distance , dist+1);
+                    
                 }
             }
 
         }
-        return distance == Integer.MIN_VALUE ? -1 : distance - 1 ;
+        return distance;
     }
 }
