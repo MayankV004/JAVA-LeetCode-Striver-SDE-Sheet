6d026029dbb1bce52cd93a4f25c796edc901b177
Mayank Verma <mayank.msverma@gmail.com>
2026-05-27 18:49:33 +0530

    Time: 6 ms (98.99%) | Memory: 61.8 MB (21.38%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,16 +1,15 @@
 class Solution {
-    public  void dfs(int[][] grid , int i , int j , int[][] visited , int m , int n){
+    public  void dfs(int[][] grid , int i , int j , int m , int n){
         // Base Case
-        if( i < 0 || j < 0 || i >= m || j >= n || visited[i][j] == 1 || grid[i][j] == 0){
+        if( i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0){
             return ;
         }        
-        visited[i][j] = 1; // marking visited
         grid[i][j] = 0;    // making the boundary 1s to 0 
 
-        dfs(grid , i , j+1 , visited , m , n );
-        dfs(grid , i , j-1 , visited , m , n );
-        dfs(grid , i+1 , j , visited , m , n );
-        dfs(grid , i-1 , j , visited , m , n );
+        dfs(grid , i , j+1 , m , n );
+        dfs(grid , i , j-1 , m , n );
+        dfs(grid , i+1 , j , m , n );
+        dfs(grid , i-1 , j , m , n );
     }
 
     public int numEnclaves(int[][] grid) {
@@ -19,11 +18,10 @@ class Solution {
 
         // first we will traverse on the boundary of grid and if there is any land on the boundary
         // then we will make it water by doing DFS 
-        int visited[][] = new int[m][n];
         for (int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 if((i == 0 || j == 0 || i == m-1 || j == n-1) && grid[i][j] == 1 ){
-                    dfs(grid , i , j , visited , m , n);
+                    dfs(grid , i , j , m , n);
                 }
             }
         }
