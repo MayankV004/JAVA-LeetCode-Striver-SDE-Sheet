e31d636cd395c8f5e4912b2efa8db225a65e650e
Mayank Verma <mayank.msverma@gmail.com>
2026-05-28 05:02:39 +0530

    Time: 53 ms (73.84%) | Memory: 127.5 MB (54.93%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -0,0 +1,81 @@
+//BFS solution 
+// class Solution {
+//     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+//         if (n == 1)
+//             return informTime[headID];
+
+//         List<List<Integer>> graph = new ArrayList<>();
+
+//         for (int i = 0; i < n; i++) {
+//             graph.add(new ArrayList<>());
+//         }
+
+//         // Graph built
+//         for (int i = 0; i < n; i++) {
+//             if (manager[i] != -1) {
+//                 graph.get(manager[i]).add(i);
+//             }
+//         }
+
+//         Queue<int[]> q = new ArrayDeque<>();
+//         // BFS queue -> {employeeID, currentTime}
+//         q.offer(new int[] { headID, 0 });
+
+//         int ans = 0;
+//         while (!q.isEmpty()) {
+//             int employee[] = q.poll();
+//             int employeeID = employee[0];
+//             int time = employee[1];
+
+//             ans = Math.max(ans, time);
+
+//             for (int subordinate : graph.get(employeeID)) {
+//                 q.offer(new int[] { subordinate, time + informTime[employeeID] });
+//             }
+//         }
+
+//         return ans;
+
+//     }
+// }
+
+class Solution {
+
+    public int dfs(int employee,
+            List<List<Integer>> adj,
+            int[] informTime) {
+
+        int maxTime = 0;
+
+        // Visit all subordinates
+        for (int child : adj.get(employee)) {
+
+            maxTime = Math.max(
+                    maxTime,
+                    dfs(child, adj, informTime));
+        }
+
+        return informTime[employee] + maxTime;
+    }
+
+    public int numOfMinutes(int n,
+            int headID,
+            int[] manager,
+            int[] informTime) {
+
+        // Build adjacency list
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (manager[i] != -1) {
+                adj.get(manager[i]).add(i);
+            }
+        }
+
+        return dfs(headID, adj, informTime);
+    }
+}
