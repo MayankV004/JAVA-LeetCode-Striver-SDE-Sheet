98d33cd12615e75000266ce049601f12c03340d6
Mayank Verma <mayank.msverma@gmail.com>
2026-05-30 00:41:58 +0530

    Time: 2 ms (75.94%) | Memory: 47.7 MB (71.94%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,42 +1,64 @@
-class Solution {
+// Problem in easy language
+// Imagine the grid is a piece of land.
+// 'X' represents walls.
+// 'O' represents open space.
+// The goal is to find all the "pools" of open space ('O') that are completely trapped by walls ('X') and fill them in (turn them into 'X').
+// However, if a pool of 'O's touches the very edge of the board (top, bottom, left, or right), it is not trapped. It has an escape route to the outside. Any 'O' connected to an edge 'O' is also safe.
 
-    private void dfs(int row,int col, char [][]board , int [][]notConvert, int m , int n ){
-        
-        if(row < 0 || col < 0 || row >= m || col >= n || board[row][col] == 'X' || notConvert[row][col] == 1){
-            return ;
+class Solution {
+    public void dfs(char[][] board, int r, int c, int m, int n, char replace) {
+        if (r < 0 || c < 0 || r > m-1 || c > n-1 || board[r][c] != 'O') {
+            return;
         }
 
-        notConvert[row][col] = 1;
-        dfs(row , col-1, board , notConvert , m , n);
-        dfs(row , col+1, board , notConvert , m , n);
-        dfs(row-1 , col, board , notConvert , m , n);
-        dfs(row+1 , col, board , notConvert , m , n);
+        board[r][c] = replace;
 
+        dfs(board, r, c - 1, m, n, replace);
+        dfs(board, r, c + 1, m, n, replace);
+        dfs(board, r - 1, c, m, n, replace);
+        dfs(board, r + 1, c, m, n, replace);
     }
 
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
-        int notConvert[][] =  new int[m][n];
-        for(int i = 0 ; i < m ; i++){ // first and last row
-            for(int j = 0 ; j < n ; j++){
-                if(i == 0 || i == m-1 || j == 0 || j == n-1){
-                    if(notConvert[i][j] == 0 && board[i][j] == 'O'){
-                        dfs(i , j , board , notConvert, m , n);
-                    }
-                }
 
+        char replace = 's';
+
+        // Basically we will do dfs on the edges of the board and find the not surrounded region
+        // and then replace the 0 with any other character while doing dfs
+        // after that we will just check of replacement character and change it again back to 0/
+
+        // left and right column
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0, m, n, replace);
+            }
+
+            if (board[i][n - 1] == 'O') {
+                dfs(board, i, n - 1, m, n, replace);
             }
         }
 
-        // now changning in the Board Grid 
+        // top and botton row
+        for (int i = 0; i < n; i++) {
+            if (board[0][i] == 'O') {
+                dfs(board, 0, i, m, n, replace);
+            }
+            if (board[m - 1][i] == 'O') {
+                dfs(board, m - 1, i, m, n, replace);
+            }
+        }
 
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(board[i][j] == 'O' && notConvert[i][j] == 0){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 's') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
             }
         }
+
     }
 }
