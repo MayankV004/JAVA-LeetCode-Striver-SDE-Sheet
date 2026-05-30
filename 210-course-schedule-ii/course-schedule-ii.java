0e19a43b19c5121e1a47a8a7d360ff44f411592d
Mayank Verma <mayank.msverma@gmail.com>
2026-05-30 11:12:00 +0530

    Time: 5 ms (84.67%) | Memory: 47 MB (43.46%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,77 +1,40 @@
 class Solution {
-    public boolean checkCycle(int node , List<List<Integer>> graph , int visited[] , int pathVisited[]){
-        visited[node] = 1;
-        pathVisited[node] = 1;
-        for(int it : graph.get(node)){
-            if(visited[it] == 0){
-                if(!checkCycle(it , graph , visited , pathVisited)) return false;
-            }else if(pathVisited[it] == 1){
-                return false; // cycle
-            }
-        }
-        pathVisited[node] = 0;
-        return true; // no cycle
-    }
-
-    public void topoSort(int node , List<List<Integer>> graph , int visited[] , Stack<Integer> st)
-    {
-        visited[node] = 1;
-        for(int it : graph.get(node)){
-            if(visited[it] == 0){
-                topoSort(it , graph , visited , st);
-            }
-        }
-        st.push(node);
-    }   
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> graph = new ArrayList<>();
-        int visited[] = new int[numCourses];
-        int pathVisited[] = new int[numCourses];
 
-        for(int i = 0 ; i < numCourses ; i++){
+        for(int i = 0 ; i < numCourses ; i ++){
             graph.add(new ArrayList<>());
         }
+        int indegree[]= new int[numCourses];
+        for(int []pre : prerequisites){
+            int u = pre[1];
+            int v = pre[0];
 
-        for(int[]edge : prerequisites){
-            int u = edge[0];
-            int v = edge[1];
-            graph.get(v).add(u);
+            graph.get(u).add(v);
+            indegree[v]++;
         }
-
-        boolean isCycle = false;
-
+        Queue<Integer> q = new ArrayDeque<>();
         for(int i = 0 ; i < numCourses ; i++){
-            if(visited[i] == 0){
-                if(!checkCycle(i , graph , visited , pathVisited)){
-                    isCycle = true; 
-                } 
+            if(indegree[i] == 0){
+                q.offer(i);
+            }
+        }
+       int []toposortOrder = new int[numCourses];
+       int idx = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            toposortOrder[idx] = node;
+            idx++;
+
+            for(int neighbor : graph.get(node)){
+                if(--indegree[neighbor] == 0){
+                    q.offer(neighbor);
+                }
             }
         }
 
-        if(isCycle){
-            return (new int[0]);
-        }
+        return idx == numCourses ? toposortOrder : new int[]{};
 
-        for(int i = 0 ; i < numCourses ; i++){
-            visited[i] = 0;
-        }
-
-        int []ans = new int[numCourses];
-        Stack<Integer> st = new Stack<>();
-
-        for(int i = 0 ; i < numCourses ; i++){
-            if(visited[i] == 0){
-                topoSort(i , graph , visited , st);
-            }
-        }
-        int i = 0;
-        while(!st.isEmpty()){
-            int temp = st.peek();
-            st.pop();
-            ans[i++] = temp; 
-        }
-
-        return ans;
 
     }
 }
