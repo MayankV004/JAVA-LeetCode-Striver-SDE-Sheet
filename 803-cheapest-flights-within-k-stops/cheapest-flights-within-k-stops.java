5b8a3c7a310d5f6ab632a9a4ee522c7df5e8668e
Mayank Verma <mayank.msverma@gmail.com>
2026-06-09 01:51:52 +0530

    Time: 5 ms (98.67%) | Memory: 47 MB (7.73%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,57 +1,27 @@
-class Pair{
-    int node , distance , stop;
-    Pair(int node , int distance , int stop){
-        this.node = node;
-        this.distance = distance;
-        this.stop = stop;
-    }
-}
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<Pair>> graph = new ArrayList<>();
-        int []dist = new int[n];
-  
-        Arrays.fill(dist , Integer.MAX_VALUE);
-    
+    public int findCheapestPrice(int n, int[][] flights, int src, int dest, int k) {
+        int dist[] = new int[n];
+
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        // using bellman ford 
+
+        // so for k Stops we need to traverse k+1 edges 
         dist[src] = 0;
-  
-        for(int i = 0 ; i < n ; i++){
-            graph.add(new ArrayList<>());
-        }
-        // Graph Creation
-        for(int []flight : flights){
-            int u = flight[0];
-            int v = flight[1];
-            int w = flight[2];
-            graph.get(u).add(new Pair(v ,  w , 0));
-        }
+        for (int i = 0; i < k + 1; i++) { // edges relaxation
+            // Snapshot : prevent using current round ’s updates within same round
+            int[] prev = Arrays.copyOf(dist, n);
+            for (int[] flight : flights) {
+                int u = flight[0];
+                int v = flight[1];
+                int cost = flight[2];
 
-        Queue<Pair> pq = new LinkedList<>();
-        pq.add(new Pair(src , 0 , 0));
-
-        while(!pq.isEmpty()){
-            int node = pq.peek().node;
-            int cost = pq.peek().distance;
-            int stop = pq.peek().stop;
-            pq.remove();
-            if(stop > k) continue;
-
-            for(Pair neighbour : graph.get(node)){
-                int neigh = neighbour.node;
-                int wt = neighbour.distance;
-                int halt = neighbour.stop;
-
-                if(wt + cost < dist[neigh] && stop <= k ){
-                    dist[neigh] = wt + cost;
-                    pq.add(new Pair(neigh , dist[neigh] , stop + 1 ));
+                if (prev[u] != Integer.MAX_VALUE && prev[u] + cost < dist[v]) {
+                    dist[v] = prev[u] + cost;
                 }
-
             }
-
         }
-        if(dist[dst] == Integer.MAX_VALUE) return -1;
 
-        return dist[dst];
-        
+        return dist[dest] != Integer.MAX_VALUE ? dist[dest] : -1;
     }
 }
