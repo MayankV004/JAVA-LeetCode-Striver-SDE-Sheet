25ce92890e3c0fd4c4a99023a556c8781534e37e
Mayank Verma <mayank.msverma@gmail.com>
2026-07-01 01:52:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (91.13%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -13,30 +13,51 @@
  *     }
  * }
  */
-class Solution {
-    public void allPaths(TreeNode root , String path , List<String> res){
-        if(root == null) return ;
+//Approach 1
+// class Solution {
+//     public void allPaths(TreeNode root , String path , List<String> res){
+//         if(root == null) return ;
 
-        path += root.val;
+//         path += root.val;
+
+//         if(root.left == null && root.right == null){
+//             res.add(path);
+//         }
+
+//         allPaths(root.left , path , res);
+//         allPaths(root.right , path , res);
+//     }
+//     public int sumNumbers(TreeNode root) {
+//         List<String> res = new ArrayList<>();
+//         allPaths(root , "" , res);
+
+//         int ans = 0 ;
+
+//         for(String s : res){
+//             ans += Integer.valueOf(s);
+//         }
+
+//         return ans;
+
+//     }
+// }
+
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int current){
+        if(root == null){
+            return 0;
+        }
+
+        current = current * 10 + root.val;
 
         if(root.left == null && root.right == null){
-            res.add(path);
+            return current;
         }
 
-        allPaths(root.left , path , res);
-        allPaths(root.right , path , res);
-    }
-    public int sumNumbers(TreeNode root) {
-        List<String> res = new ArrayList<>();
-        allPaths(root , "" , res);
-
-        int ans = 0 ;
-
-        for(String s : res){
-            ans += Integer.valueOf(s);
-        }
-
-        return ans;
-
+        return dfs(root.left, current) + dfs(root.right , current);
     }
 }
