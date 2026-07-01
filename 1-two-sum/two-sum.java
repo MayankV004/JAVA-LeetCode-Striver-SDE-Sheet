dd977c77df3d9f340897a0d3c3fa6b223acafe8b
Mayank Verma <mayank.msverma@gmail.com>
2026-07-01 21:50:52 +0530

    Time: 4 ms (50.55%) | Memory: 46.8 MB (89.55%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap <Integer , Integer> map = new HashMap<>();
-        int [] ans = new int [2];
-        for (int i = 0 ; i < nums.length ; i++)
-        {
-            if (map.containsKey(nums[i]))
-            {
-                int idx = map.get(nums[i]);
-                ans[0] = idx;
-                ans[1] = i;
-            }
-            else
-            {
-                map.put(target - nums[i] , i);
+        Map<Integer , Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i++){
+            map.put(nums[i] , i);
+        }
+        int []ans = new int[2];
+        for(int i = 0 ; i < nums.length ; i++){
+            int curr = nums[i];
+            int remaining = target - curr ;
+
+            if(map.containsKey(remaining) && map.get(remaining) != i){
+                ans[0] = i;
+                ans[1] = map.get(remaining);
+                break;
             }
         }
         return ans;
