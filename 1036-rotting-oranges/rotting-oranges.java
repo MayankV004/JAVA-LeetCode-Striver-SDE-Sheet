e680caff082e451a5c9455d3fb39bf2609dea470
Mayank Verma <mayank.msverma@gmail.com>
2026-07-02 10:39:46 +0530

    Time: 2 ms (90.83%) | Memory: 44.1 MB (72.16%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,65 +1,51 @@
-class Pair{
-    int row , col , time;
-
-    public Pair(int row , int col , int time){
-        this.row = row ;
-        this.col = col ; 
-        this.time = time ;
-    }
-}
-
 class Solution {
     public int orangesRotting(int[][] grid) {
         
-        int m = grid.length ;
-        int n = grid[0].length ;
-        Queue<Pair> q = new LinkedList<>();
-        int [][]visited = new int[m][n];
-
-        // counting fresh and adding rotten into the queue for initial filling of queue
         int fresh = 0;
+        int m = grid.length ;
+        int n = grid[0].length;
+        Queue<int[]> q = new ArrayDeque<>();
+        int visited[][] = new int[m][n];
         for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-
-                if(grid[i][j] == 2){
-                    q.offer(new Pair(i , j , 0)); // adding this to queue
-                    visited[i][j] = 1; // marking visited
-                }
-
+            for(int j = 0 ; j < n ; j++ ){
                 if(grid[i][j] == 1){
                     fresh++;
                 }
+                if(grid[i][j] == 2){
+                    q.offer(new int[]{i , j , 0}); // row , col , time
+                    visited[i][j] = 1;
+                }
             }
         }
 
-        int time = 0;
-        int []delRow = {0 , 0 , -1 , 1};
-        int []delCol = {-1 , 1 , 0 , 0};
-        int count = 0;
+        if(fresh == 0){
+            return 0;
+        }
+
+        // Multi source BFS
+        int directions[][] = { {-1 , 0} , {1 , 0}, {0 , -1} , { 0 , 1}}; // all four directions
+        int countRotten = 0;
+        int ans = 0;
         while(!q.isEmpty()){
-            int r = q.peek().row;
-            int c = q.peek().col;
-            int t = q.peek().time;
-            q.poll();
+            int current[] = q.poll();
 
-            time = Math.max(time , t); // setting new Time
+            int row = current[0];
+            int col = current[1];
+            int time = current[2];
+            ans = Math.max(ans , time);
+            for(int dirs[] : directions){
+                int nr = row + dirs[0];
+                int nc = col + dirs[1];
 
-            // new row and col for adjacent elements 
-
-            for(int i = 0 ; i < 4 ; i ++){
-                int nrow = r + delRow[i];
-                int ncol = c + delCol[i];
-                
-                if(nrow >= 0 && ncol >= 0 && nrow < m && ncol < n && visited[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
-                    // rotting the orange
-                    q.offer(new Pair(nrow , ncol , t+1));
-                    visited[nrow][ncol] = 1 ;
-                    count++;
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n && visited[nr][nc] == 0 && grid[nr][nc] == 1){
+                    countRotten++;
+                    visited[nr][nc] = 1;
+                    q.offer(new int[]{nr , nc , time+1});
                 }
             }
 
         }
-        return count == fresh ? time : -1;
 
+        return countRotten == fresh ? ans : -1;
     }
 }
