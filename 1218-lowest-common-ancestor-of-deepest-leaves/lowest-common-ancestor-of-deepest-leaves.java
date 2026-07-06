4a4272c7dd54ae8773b7cff38216d64d7416eaf1
Mayank Verma <mayank.msverma@gmail.com>
2026-07-06 23:04:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (47.91%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -1,53 +1,103 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// Approach 2
 class Solution {
+    class NodeInfo{
+        TreeNode node ;
+        int depth;
 
-    public TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null)
-            return null;
-
-        if (root == p || root == q)
-            return root;
-
-        TreeNode left = lca(root.left, p, q);
-        TreeNode right = lca(root.right, p, q);
-
-        if (left != null && right != null)
-            return root;
-
-        return left != null ? left : right;
+        public NodeInfo(int depth , TreeNode node){
+            this.depth = depth;
+            this.node = node;
+        } 
     }
+    public NodeInfo lca(TreeNode root){
+        if(root == null){
+            return new NodeInfo( 0, root);
+        }
 
+        NodeInfo left = lca(root.left);
+        NodeInfo right = lca(root.right);
+
+        if(left.depth == right.depth){
+            return new NodeInfo(left.depth + 1 , root);
+        }else if(left.depth > right.depth){
+            return new NodeInfo(left.depth + 1 , left.node);
+        }else{
+            return new NodeInfo(right.depth + 1 , right.node);
+        }
+    }
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        if (root == null)
-            return null;
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        List<TreeNode> deepest = new ArrayList<>();
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            deepest = new ArrayList<>();
-
-            while (size-- > 0) {
-                TreeNode node = q.poll();
-                deepest.add(node);
-
-                if (node.left != null)
-                    q.offer(node.left);
-
-                if (node.right != null)
-                    q.offer(node.right);
-            }
-        }
-
-        // Compute LCA of all deepest leaves
-        TreeNode ans = deepest.get(0);
-
-        for (int i = 1; i < deepest.size(); i++) {
-            ans = lca(root, ans, deepest.get(i));
-        }
-
-        return ans;
+        NodeInfo ans = lca(root);
+        return ans.node;
     }
 }
+
+// Approach 1 
+// class Solution {
+
+//     public TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
+//         if (root == null)
+//             return null;
+
+//         if (root == p || root == q)
+//             return root;
+
+//         TreeNode left = lca(root.left, p, q);
+//         TreeNode right = lca(root.right, p, q);
+
+//         if (left != null && right != null)
+//             return root;
+
+//         return left != null ? left : right;
+//     }
+
+//     public TreeNode lcaDeepestLeaves(TreeNode root) {
+//         if (root == null)
+//             return null;
+
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+
+//         List<TreeNode> deepest = new ArrayList<>();
+
+//         while (!q.isEmpty()) {
+//             int size = q.size();
+//             deepest = new ArrayList<>();
+
+//             while (size-- > 0) {
+//                 TreeNode node = q.poll();
+//                 deepest.add(node);
+
+//                 if (node.left != null)
+//                     q.offer(node.left);
+
+//                 if (node.right != null)
+//                     q.offer(node.right);
+//             }
+//         }
+
+//         // Compute LCA of all deepest leaves
+//         TreeNode ans = deepest.get(0);
+
+//         for (int i = 1; i < deepest.size(); i++) {
+//             ans = lca(root, ans, deepest.get(i));
+//         }
+
+//         return ans;
+//     }
+// }
