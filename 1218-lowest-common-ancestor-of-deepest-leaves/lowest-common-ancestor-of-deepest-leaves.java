0e7880ea3c4eee4aa84fae6b088921c176d9222c
Mayank Verma <mayank.msverma@gmail.com>
2026-07-06 06:28:30 +0530

    Time: 2 ms (21.01%) | Memory: 46.5 MB (31.51%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,53 @@
+class Solution {
+
+    public TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return null;
+
+        if (root == p || root == q)
+            return root;
+
+        TreeNode left = lca(root.left, p, q);
+        TreeNode right = lca(root.right, p, q);
+
+        if (left != null && right != null)
+            return root;
+
+        return left != null ? left : right;
+    }
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        if (root == null)
+            return null;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        List<TreeNode> deepest = new ArrayList<>();
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            deepest = new ArrayList<>();
+
+            while (size-- > 0) {
+                TreeNode node = q.poll();
+                deepest.add(node);
+
+                if (node.left != null)
+                    q.offer(node.left);
+
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+        }
+
+        // Compute LCA of all deepest leaves
+        TreeNode ans = deepest.get(0);
+
+        for (int i = 1; i < deepest.size(); i++) {
+            ans = lca(root, ans, deepest.get(i));
+        }
+
+        return ans;
+    }
+}
