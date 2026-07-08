40023bf157d54e135af0e13837f931112caa03b5
Mayank Verma <mayank.msverma@gmail.com>
2026-07-08 17:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (45.22%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -15,17 +15,37 @@
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
-        if (root == null)
-            return 0;
-        int sum = 0;
-        if (root.val >= low && root.val <= high)
-            sum += root.val;
+        if(root == null) return 0;
 
-        if (root.val > low)
-            sum += rangeSumBST(root.left, low, high);
-        if (root.val < high)
-            sum += rangeSumBST(root.right, low, high);
+        if(root.val >= low && root.val<= high){
+            // got one value , therefore going in both left and right
+            return root.val + rangeSumBST(root.left , low , high) + rangeSumBST(root.right, low , high);
+        }
 
-        return sum;
+        // when value is less than low so no point in going left 
+        if(root.val < low){
+            return rangeSumBST(root.right , low , high);
+        }
+
+        // when value is higher than high so no point in going right 
+        return rangeSumBST(root.left , low , high);
     }
 }
+
+//Approach 2
+// class Solution {
+//     public int rangeSumBST(TreeNode root, int low, int high) {
+//         if (root == null)
+//             return 0;
+//         int sum = 0;
+//         if (root.val >= low && root.val <= high)
+//             sum += root.val;
+
+//         if (root.val > low)
+//             sum += rangeSumBST(root.left, low, high);
+//         if (root.val < high)
+//             sum += rangeSumBST(root.right, low, high);
+
+//         return sum;
+//     }
+// }
