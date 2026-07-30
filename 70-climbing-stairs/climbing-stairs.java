5e936cd8e5fbabad5e0ff272b9e11058ea27669c
Mayank Verma <mayank.msverma@gmail.com>
2026-07-31 01:47:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (55.16%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int helper(int dp[] , int n){
+        if(n <= 1){
+            return 1;
+        }
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = helper(dp , n-1) + helper(dp , n-2);
+    }
+    public int climbStairs(int n) {
+        if(n < 2 ) return 1;
+
+        int dp[] = new int[n+1];
+
+        Arrays.fill(dp , -1);
+        return helper(dp , n);
+    }
+}
