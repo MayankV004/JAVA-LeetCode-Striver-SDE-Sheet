4a213e6ee6216115e6e61af5dbb23541970b3974
Mayank Verma <mayank.msverma@gmail.com>
2026-07-31 20:06:40 +0530

    Time: 45 ms (9.65%) | Memory: 47.2 MB (76.64%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,20 +1,38 @@
+// class Solution {
+//     public int jump(int[] nums) {
+
+//         int jumps = 0;
+//         int currentEnd = 0;
+//         int farthest = 0;
+
+//         for (int i = 0; i < nums.length - 1; i++) {
+
+//             farthest = Math.max(farthest, i + nums[i]);
+
+//             if (i == currentEnd) {
+//                 jumps++;
+//                 currentEnd = farthest;
+//             }
+//         }
+
+//         return jumps;
+//     }
+// }
+
 class Solution {
     public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
 
-        int jumps = 0;
-        int currentEnd = 0;
-        int farthest = 0;
-
-        for (int i = 0; i < nums.length - 1; i++) {
-
-            farthest = Math.max(farthest, i + nums[i]);
-
-            if (i == currentEnd) {
-                jumps++;
-                currentEnd = farthest;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == Integer.MAX_VALUE) continue; // unreachable
+            for (int j = 1; j <= nums[i] && i + j < n; j++) {
+                dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
             }
         }
 
-        return jumps;
+        return dp[n - 1];
     }
 }
