4e75b207e3a2fe0decef362f6ce7dacb590d8348
Mayank Verma <mayank.msverma@gmail.com>
2026-08-02 17:11:05 +0530

    Time: 13 ms (14.41%) | Memory: 46.1 MB (97.22%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,35 +1,56 @@
-class Solution {
-    Map<Integer , Integer> map = new HashMap<>();
-    int dp[];
-    public int solve(int n){
-        if(n== 0){
-            return dp[n] = 0;
-        }
-        if(n == 1) {
-            return dp[n] = map.getOrDefault(1 , 0);
-        }
+// class Solution {
+//     Map<Integer , Integer> map = new HashMap<>();
+//     int dp[];
+//     public int solve(int n){
+//         if(n== 0){
+//             return dp[n] = 0;
+//         }
+//         if(n == 1) {
+//             return dp[n] = map.getOrDefault(1 , 0);
+//         }
 
-        if(dp[n] != -1) return dp[n];
+//         if(dp[n] != -1) return dp[n];
 
-        return dp[n] = Math.max(map.getOrDefault(n , 0) + solve(n-2) , solve(n-1));
-    }
-    public int deleteAndEarn(int[] nums) {
-        // the recurrence relatetion we will use here is 
-        // f(n) = Max(gain + f(n-2) , f(n-1));
-        // and we will start from max n;
+//         return dp[n] = Math.max(map.getOrDefault(n , 0) + solve(n-2) , solve(n-1));
+//     }
+//     public int deleteAndEarn(int[] nums) {
+//         // the recurrence relatetion we will use here is 
+//         // f(n) = Max(gain + f(n-2) , f(n-1));
+//         // and we will start from max n;
 
-        int n = nums.length;
+//         int n = nums.length;
 
-        int max = 0;
-        for(int num : nums){
-            max = Math.max(max , num);
-            map.put(num , map.getOrDefault(num , 0) + num);
-        }
-        dp = new int[max+1];
-        Arrays.fill(dp , -1);
-        return solve(max);
+//         int max = 0;
+//         for(int num : nums){
+//             max = Math.max(max , num);
+//             map.put(num , map.getOrDefault(num , 0) + num);
+//         }
+//         dp = new int[max+1];
+//         Arrays.fill(dp , -1);
+//         return solve(max);
         
 
 
+//     }
+// }
+
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            max = Math.max(max, num);
+            map.put(num, map.getOrDefault(num, 0) + num);
+        }
+
+        int[] dp = new int[max + 1];
+        dp[0] = 0;
+        dp[1] = map.getOrDefault(1, 0);
+
+        for (int i = 2; i <= max; i++) {
+            dp[i] = Math.max(map.getOrDefault(i, 0) + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[max];
     }
 }
