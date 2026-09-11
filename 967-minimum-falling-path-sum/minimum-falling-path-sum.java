3a323c885691704a4823d33bb5bc565520d06cf1
Mayank Verma <mayank.msverma@gmail.com>
2026-09-11 18:37:47 +0530

    Time: 1 ms (99.87%) | Memory: 47.2 MB (64.28%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,39 +1,35 @@
 class Solution {
-    public int minFallingPathSum(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length ;
+    int m , n;
+    public int solve(int i , int j , int dp[][] , int matrix[][]){
+        if(j < 0 || j >= n ){
+            return Integer.MAX_VALUE;
+        }
+        if(i == m-1){
+            return matrix[i][j];
+        }
+        if(dp[i][j] != Integer.MAX_VALUE) return dp[i][j];
 
-        int dp [][] = new int[m][n];
+        int leftDiag = solve( i+1 , j-1 , dp , matrix);
+        int down = solve( i+1 , j , dp , matrix);
+        int rightDiag = solve( i+1 , j+1 , dp , matrix);
 
-        for(int j = 0 ; j < n ; j++){  // dp initailization
-            dp[0][j] = matrix[0][j];
+        return dp[i][j] = matrix[i][j] + Math.min(rightDiag , Math.min(leftDiag , down));
+    }
+    public int minFallingPathSum(int[][] matrix) { 
+        m = matrix.length;
+        n = matrix[0].length;
+
+        int dp[][] = new int[m][n];
+
+        for(int i = 0 ; i < m ;i++){
+            Arrays.fill(dp[i] , Integer.MAX_VALUE);
         }
 
-        for(int i = 1 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++ ){
-                int up = matrix[i][j] + dp[i-1][j];
-                int leftDiag = (int)1e9; // because if move it not possible the it should not affect the outcome
-                int rightDiag = (int)1e9;
-                if(j > 0){
-                    leftDiag = matrix[i][j]+  dp[i-1][j-1];
-                } 
-
-                if(j < n-1 ){
-                    rightDiag = matrix[i][j] + dp[i-1][j+1];
-                }
-
-                dp[i][j] = Math.min(up , Math.min(leftDiag , rightDiag));
-            }
-        }
-        int ans = (int)1e9;
+        int mini = Integer.MAX_VALUE;
         for(int j = 0 ; j < n ; j++){
-            System.out.println(dp[m-1][j]);
-            if(dp[m-1][j] < ans){
-            
-                ans = dp[m-1][j];
-            }
+            mini = Math.min(mini , solve(0 , j , dp , matrix));
         }
 
-        return ans;
+        return mini;
     }
 }
